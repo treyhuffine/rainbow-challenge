8db3f8566fcbd229043f9d602efffbf4cc23dd2d
Trey Huffine <treyhuffine@gmail.com>
2015-08-07 10:02:09 -0700

    Update rainbow styling

--- src/javascripts/rainbow/SingleArch.jsx
+++ src/javascripts/rainbow/SingleArch.jsx
@@ -9,7 +9,8 @@ export default class SingleArch extends React.Component {
       'borderTopLeftRadius': '100vw',
       'borderTopRightRadius': '100vw',
       'backgroundColor': this.props.rainbowSpecs,
-      'bottom': '15vh'
+      'bottom': '15vh',
+      'left': `${this.props.sizing/30}vw`
     }
     return (
       <div style={archStyle}></div>
