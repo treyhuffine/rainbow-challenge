970b744db9ba1a0df33818cfa9285fe3d7133038
Trey Huffine <treyhuffine@gmail.com>
2015-08-07 12:08:40 -0700

    Update sparkles

--- src/javascripts/sparkles/SingleSparkle.jsx
+++ src/javascripts/sparkles/SingleSparkle.jsx
@@ -7,10 +7,15 @@ export default class SingleSparkle extends React.Component {
       'position': 'absolute',
       'color': 'white',
       'left': `${Math.random() * 50}vw`,
-      'top': `${Math.random() * 20}vh`
+      'top': `${Math.random() * 20}vh`,
+      'width': '5px',
+      'height': '5px',
+      'borderRadius': '50%',
+      'backgroundColor': 'white',
+      'boxShadow': '0 0 5px rgba(255, 255, 255, 1)'
     }
     return (
-      <p style={sparkleStyle} className='sparkle'>sparkle</p>
+      <div style={sparkleStyle} className='sparkle'></div>
     )
   }
 }
