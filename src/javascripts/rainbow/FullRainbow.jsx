963028352fb21c3b2a315a6dfcfa1bee90be92ec
Trey Huffine <treyhuffine@gmail.com>
2015-08-07 10:03:20 -0700

    Remove spurious styling code

--- src/javascripts/rainbow/FullRainbow.jsx
+++ src/javascripts/rainbow/FullRainbow.jsx
@@ -1,27 +1,5 @@
 import React from 'react';
-
-import SingleArch from './SingleArch'
-
-let firstRainbow = {
-  'position': 'absolute',
-  'bottom': '10vh',
-  'left': '20vw',
-  'borderTopLeftRadius': '100vh',
-  'borderTopRightRadius': '100vh',
-  'width': '20vw',
-  'height': '10vw',
-  'backgroundColor': '#7fc6f4'
-}
-let secondRainbow = {
-  'position': 'absolute',
-  'bottom': '10vh',
-  'left': '15vw',
-  'borderTopLeftRadius': '100vh',
-  'borderTopRightRadius': '100vh',
-  'width': '30vw',
-  'height': '15vw',
-  'backgroundColor': 'red'
-}
+import SingleArch from './SingleArch';
 
 export default class FullRainbow extends React.Component {
 
