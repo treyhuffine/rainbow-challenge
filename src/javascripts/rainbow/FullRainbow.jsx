932724701ae7cfb4135dbe55c47125da546b4f68
Trey Huffine <treyhuffine@gmail.com>
2015-08-07 09:27:36 -0700

    Add basic rainbow arch

--- src/javascripts/rainbow/FullRainbow.jsx
+++ src/javascripts/rainbow/FullRainbow.jsx
@@ -1,9 +1,33 @@
 import React from 'react';
 
+let firstRainbow = {
+  'position': 'absolute',
+  'bottom': '10vh',
+  'left': '20vw',
+  'borderTopLeftRadius': '100vh',
+  'borderTopRightRadius': '100vh',
+  'width': '20vw',
+  'height': '10vw',
+  'backgroundColor': '#7fc6f4'
+}
+let secondRainbow = {
+  'position': 'absolute',
+  'bottom': '10vh',
+  'left': '15vw',
+  'borderTopLeftRadius': '100vh',
+  'borderTopRightRadius': '100vh',
+  'width': '30vw',
+  'height': '15vw',
+  'backgroundColor': 'red'
+}
+
 export default class FullRainbow extends React.Component {
   render() {
     return (
-      <h1>Hello Rainbow</h1>
+      <div id="rainbow-wrapper">
+        <div style={secondRainbow}></div>
+        <div style={firstRainbow}></div>
+      </div>
     )
   }
 }
